f1f0062d50d649bbc25f4efa86ec870e6f3a99b8
Noa Cohen <noa.cohen04@post.runi.ac.il>
2024-01-04 08:09:16 +0200

    homework 2

--- OneOfEach.java
+++ OneOfEach.java
@@ -4,7 +4,32 @@
  *  to have children until they have at least one child of each gender.
  */
 public class OneOfEach {
-	public static void main (String[] args) {
-		//// Put your code here
+	public static void main (String[] args)
+	{
+	 boolean boy = false;
+	 boolean girl = false;
+	 int sum = 0;
+	 int a = 0;
+
+	 	 while (boy == false || girl == false )
+	 	 {
+	 	 	 a = (int)( 2 * Math.random() ) ;
+	 	 	 if (a == 0)
+	 	 	 {
+	 	 	 	boy = true;
+	 	 	 	System.out.print("b ");
+
+	 	 	 }
+	 	 	 else
+	 	 	 {
+	 	 	 	girl = true;
+	 	 	 	System.out.print("g ");
+	 	 	 }
+	 	 	 sum +=1;
+
+	 	 }
+
+	 System.out.println("\nYou made it... and you now have " + sum + " children.");
+
 	}
 }
--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -13,7 +13,77 @@ public class OneOfEachStats {
 		int T = Integer.parseInt(args[0]);
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
-        Random generator = new Random(seed);  
+        Random generator = new Random(seed); 
+        double rnd = generator.nextDouble();
+
+
+
+
+		boolean boy = false;
+		boolean girl = false;
+		int sum = 0;//children in each family
+	    int sum2 = 0;
+	    int sum3 = 0;
+	    int sum4 = 0;
+	    int a = 0;
+	    int allChildren = 0;//all the children
+	    double average = 0;
+	    String common = "wrong";
+
+		for (int i =0 ; i< T ; i++)
+		{
+			 boy = false;
+			 girl = false;
+			 sum = 0;
+
+		 while (boy == false || girl == false )
+	 	 {
+	 	    a = (int)( 2 * generator.nextDouble()) ;
+	 	 	if (a == 0)
+	 	 	 {
+	 	 	 	boy = true;
+
+	 	 	 }
+	 	 	 else
+	 	 	 {
+	 	 	 	girl = true;
+	 	 	 }
+	 	 	 sum +=1;
+	 	 	 allChildren ++;
+	 	 }
+
+	 	 if (sum>= 4)
+	 	 	sum4 ++;
+	 	 else
+	 	 {
+	 	 	if(sum == 2)
+	 	 		sum2 ++;
+	 	 	else
+	 	 		sum3++;
+	 	 }
+	    }
+
+	    
+	    if ((sum4>sum2)&&(sum4>sum3))
+	    		common = "4 or more";
+	    else
+	    {
+	    	if (((sum3>sum2)&&(sum3>sum4)) || ((sum3>sum2)&& (sum3==sum4)) )
+	    		common = "3";
+	    	else
+	    	{
+	    	     common = "2";
+	       }
+	    }
+
+	average = (double)allChildren/T;
+	      System.out.println("Average: " + average + " children to get at least one of each gender.");
+	      System.out.println("Number of families with 2 children: " + sum2);
+	      System.out.println("Number of families with 3 children: " + sum3);
+	      System.out.println("Number of families with 4 or more children: " + sum4);
+          System.out.println("The most common number of children is " + common + ".");
+
+
 		
 		//// In the previous version of this program, you used a statement like:
 		//// double rnd = Math.random();
